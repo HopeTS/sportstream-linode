7106b6ca78e47009cece621f737249679ddb977d
Robert Bergers <bergersr97@gmail.com>
2020-10-30 17:48:36 -0400

    customize cert script
    Rename org and server name, and remove client certificate

--- server/ssl/generate-certificates.sh
+++ server/ssl/generate-certificates.sh
@@ -2,10 +2,10 @@
 
 # set values for certificate DNs
 # note: CN is set to different values in the sections below
-ORG="000_Test_Certificates"
+ORG="DabbleCast"
 
 # set values that the commands will share
-VALID_DAYS=360
+VALID_DAYS=30
 CA_KEY=ca.key
 CA_CERT=ca.crt
 CLIENT_KEY=client.key
@@ -19,30 +19,15 @@ KEY_BITS=2048
 
 echo
 echo "Create CA certificate..."
-CN="Test CA"
+CN="DabbleCast CA"
 openssl genpkey -algorithm RSA -pkeyopt rsa_keygen_bits:$KEY_BITS -out $CA_KEY
 openssl req -new -x509 -days $VALID_DAYS -key $CA_KEY -subj "/CN=$CN/O=$ORG" -out $CA_CERT
 echo "Done."
 
 echo
 echo "Creating Server certificate..."
-CN="localhost"
+CN="DabbleCast server"
 openssl genpkey -algorithm RSA -pkeyopt rsa_keygen_bits:$KEY_BITS -out $SERVER_KEY
 openssl req -new -key $SERVER_KEY -subj "/CN=$CN/O=$ORG" -out $SERVER_CSR
 openssl x509 -days $VALID_DAYS -req -in $SERVER_CSR -CAcreateserial -CA $CA_CERT -CAkey $CA_KEY -out $SERVER_CERT
 echo "Done."
-
-echo
-echo "Creating Client certificate..."
-CN="Test User 1"
-USER_ID="testuser1"
-P12_PASSWORD=
-openssl genpkey -algorithm RSA -pkeyopt rsa_keygen_bits:$KEY_BITS -out $CLIENT_KEY
-openssl req -new -key $CLIENT_KEY -subj "/CN=$CN/O=$ORG/UID=$USER_ID" -out $CLIENT_CSR
-openssl x509 -days $VALID_DAYS -req -in $CLIENT_CSR -CAcreateserial -CA $CA_CERT -CAkey $CA_KEY -out $CLIENT_CERT
-openssl pkcs12 -in $CLIENT_CERT -inkey $CLIENT_KEY -export -password pass:$P12_PASSWORD -out $CLIENT_P12
-echo "Done."
-
-echo
-echo "----- Don't forget to open your browser and install your $CA_CERT and $CLIENT_P12 certificates -----"
-echo
