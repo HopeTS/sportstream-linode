967d0e704f935aa74aff0301570941e7dea9f542
Robert Bergers <bergersr97@gmail.com>
2020-10-31 12:11:08 -0400

    Connect nodejs server to MongoDB with mongoose

--- server/server.js
+++ server/server.js
@@ -5,25 +5,42 @@ const http = require('http');
 const path = require('path');
 const fs = require('fs');
 const chalk = require('chalk');
+const mongoose = require('mongoose');
 
 
 /* Local files */
 const publicPath = path.join(__dirname, '../public');
 const mainRouter = require('./routers/app');
 const config = require('./config/default');
+
 const RenewSSLCert = require('./cron/renew-ssl-cert');
 const MongoD = require('./database/mongod');
 
 
-/* Cron jobs */
-const ssl_cron = new RenewSSLCert;
-
-
 /* Environment variables */
 const http_port = config.http.port;
+const mongodb_port = config.mongodb.port;
+const mongodb_path = config.mongodb.path;
 const env = process.env.NAME;
 
 console.log(chalk.bold('Environment:'), chalk.green(process.env.NAME));
+
+
+/* Connect to MongoDB */
+mongod = new MongoD(config.mongodb);
+mongod.create_connection();
+mongoose.connect(
+    `mongodb://localhost:${mongodb_port}/sportstream-linode`,
+    {useNewUrlParser: true}
+);
+const db = mongoose.connection;
+db.once('open', () => {
+    console.log(chalk.green('Mongoose has connected to MongoDB'))
+})
+
+
+/* Cron jobs */
+const ssl_cron = new RenewSSLCert;
 
 
 /* Configure express */
@@ -31,11 +48,6 @@ const app = express();
 app.use(express.static(publicPath));
 app.use(express.json());
 app.use(mainRouter);
-
-
-/* Connect to MongoDB */
-mongod = new MongoD(config.mongodb);
-mongod.create_connection();
 
 
 /* Run server */
