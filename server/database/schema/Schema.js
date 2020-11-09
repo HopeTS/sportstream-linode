090e622a32c0038460d6c8991fae4170ccbf386c
Robert Bergers <bergersr97@gmail.com>
2020-11-09 10:21:03 -0500

    Refactor: One schema for user and business accounts

--- server/database/schema/Schema.js
+++ server/database/schema/Schema.js
@@ -1,4 +1,3 @@
 const mongoose = require('mongoose');
 
 exports.User = mongoose.model('User', require('./UserSchema'));
-exports.Business = mongoose.model('Business', require('./BusinessSchema'));
--- server/database/schema/UserSchema.js
+++ server/database/schema/UserSchema.js
@@ -7,15 +7,20 @@ const UserSchema = new Schema({
     name: String,
     email: String,
     password: String,
+    stream_key: String,
     business: Schema.Types.ObjectId,
 });
 
 UserSchema.methods.generateHash = (password) => {
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(), null);
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
 };
 
 UserSchema.methods.validPassword = (password) => {
     return bcrypt.compareSync(password, this.password);
 };
+
+UserSchema.methods.generateStreamKey = () => {
+    return shortid.generate();
+}
 
 module.exports = UserSchema;
