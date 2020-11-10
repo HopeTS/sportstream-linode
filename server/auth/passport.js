a3dc6577063c9df40f28df6e1091025d067b0fa4
Robert Bergers <bergersr97@gmail.com>
2020-11-10 08:33:04 -0500

    BUG FIX!

--- server/auth/passport.js
+++ server/auth/passport.js
@@ -2,39 +2,35 @@ const User = require('../database/schema/Schema').User;
 const bcrypt = require('bcryptjs');
 const localStrategy = require('passport-local').Strategy;
 
-module.exports = function(passport) {
+module.exports = function (passport) {
 
-    passport.use(
-        new localStrategy((email, password, done) => {
-
-            User.findOne({email: email}, (err, user) => {
-                if (err) throw err;
-                if (!user) return done(null, user);
-                else {
-                    bcrypt.compare(password, user.password, (err, result) => {
-                        if (err) throw err;
-                        if (result === true) {
-                            return done(null, user);
-                        } else {
-                            return done(null, false);
-                        }
-                    });
-                    return done(null, user);
-                }
-            });
-        })
-    );
-
-    passport.serializeUser((user, cb) => {
-        cb(null, user.id);
-    });
-    
-    passport.deserializeUser((id, cb) => {
-        User.findOne({_id: id}, (err, user) => {
-            const userInformation = {
-                email: user.email
-            };
-            cb(err, userInformation);
+  passport.use(
+    new localStrategy((username, password, done) => {
+        
+      User.findOne({ username: username }, (err, user) => {
+        if (err) throw err;
+        if (!user) return done(null, false);
+        bcrypt.compare(password, user.password, (err, result) => {
+          if (err) throw err;
+          if (result === true) {
+            return done(null, user);
+          } else {
+            return done(null, false);
+          }
         });
+      });
+    })
+  );
+
+  passport.serializeUser((user, cb) => {
+    cb(null, user.id);
+  });
+  passport.deserializeUser((id, cb) => {
+    User.findOne({ _id: id }, (err, user) => {
+      const userInformation = {
+        username: user.username,
+      };
+      cb(err, userInformation);
     });
-}
+  });
+};
