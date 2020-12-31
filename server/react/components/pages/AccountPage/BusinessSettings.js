ba273d5fcb082d00be48b9de0e12ee8ac4159082
Robert Bergers <robby@mymedhat.com>
2020-12-31 10:40:26 -0500

    Add Business account settings page template

---
+++ server/react/components/pages/AccountPage/BusinessSettings.js
@@ -0,0 +1,131 @@
+import React from 'react';
+import {connect} from 'react-redux';
+
+export function BusinessSettings() {
+
+    /**
+     * Handles email change form submission
+     */
+    const handleEmailChange = (e) => {
+        e.preventDefault();
+        // TODO: Validate form details
+        // TODO: Reset input and show error response if invalid input
+        // TODO: Form axios request
+        // TODO: Handle axios error
+        // TODO: Handle axios response
+    }
+
+    /**
+     * Handles password change form submission
+     */
+    const handlePasswordChange = (e) => {
+        e.preventDefault();
+        // TODO: Validate form details
+        // TODO: Reset input and show error response if invalid input
+        // TODO: Form axios request
+        // TODO: Handle axios error
+        // TODO: Handle axios response
+    }
+
+    /**
+     * Handles stream key change form submission
+     */
+    const handleStreamKeyChange = (e) => {
+        e.preventDefault();
+        // TODO: Validate form details
+        // TODO: Reset input and show error response if invalid input
+        // TODO: Form axios request for adding stream key
+        // TODO: Handle axios error for adding stream key
+        // TODO: Handle axios response for adding stream key
+        // TODO: Form axios request for deleting stream key
+        // TODO: Handle axios error for deleting stream key
+        // TODO: Handle axios response for deleting stream key
+    }
+
+    return (
+        <div className="AccountPageSettings">
+            <div className="AccountPageSettings__section">
+                <form 
+                    action="/change-email" 
+                    className="AccountPageSettings__form"
+                    id="change_email"
+                >
+                    <section>
+                        <label htmlFor="email">
+                            Current email:
+                        </label>
+
+                        <input 
+                            id="email"
+                            name="email"
+                            type="text"
+                            placeholder={props.account.email}
+                        />
+
+                        <button
+                            onClick={handleEmailChange}
+                        >
+                            Change email
+                        </button>
+                    </section>
+                    {/* 
+                        TODO: Email section 
+                        Contains current email
+                        Form to change email
+                        Submit button
+                    */}
+                </form>
+            </div>
+
+            <div className="AccountPageSettings__section">
+                <form 
+                    action="/change-password"
+                    id="change_password"
+                    className="AccountPageSettings__form"
+                >
+                    {/* 
+                        TODO: Password section  
+                        Contains option to change password
+                        Submit button
+                    */}
+                </form>
+            </div>
+
+            <div className="AccountPageSettings__section">
+                <form 
+                    action="handle-keys" 
+                    className="AccountPageSettings__form"
+                >
+                    {/* 
+                        TODO: Stream key section 
+                        Contains current stream keys
+                        Option to delete keys
+                        Option to add keys
+                        (MVP VITAL)
+                    */}
+                </form>
+            </div>
+
+            <div className="AccountPageSettings__section">
+                <form 
+                    action="" 
+                    className="AccountPageSettings__form"
+                >
+                    {/*
+                        TODO: Connected users section
+                        (Show ten, then a 'show more' option)
+                    */}
+                </form>
+            </div>
+        </div>
+    );
+}
+
+const mapStateToProps = (state) => {
+    return {
+        isAuthenticated: state.auth.isAuthenticated,
+        account: state.auth.account
+    }
+}
+
+export default connect(mapStateToProps, undefined)(BusinessSettings);
