be9f182c163cfe7e5febc3a32aee8e7cb3c3bf02
RobbyB97 <bergersr97@gmail.com>
2021-01-07 04:02:30 -0500

    Add keygen function for generating a business' connection id
    Connection IDs have to be unique by nature, only one per business. It would be overly complicated to allow the connection ID to be set specifically by the business, so it is generated randomly through this function

--- server/auth/keygen.js
+++ server/auth/keygen.js
@@ -1,5 +1,8 @@
+const { User, Business } = require('../database/schema/Schema');
+
 /**
  * Generate stream key
+ * 
  * @param {number} length length of the stream key
  *
  * @returns String
@@ -17,4 +20,44 @@ const generateStreamKey = (length=12) => {
     return streamKeyArray.join('');
 }
 
+/**
+ * Generate connection ID
+ * 
+ * @param {string} length length of the stream key
+ * 
+ * @returns string
+ */
+const generateConnectionID = (length=8) => {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let connectionIDArray = [];
+    let validID = false;
+    let connectionID = '';
+
+    // Generate connection ID strings until a valid unique one is found.
+    while (!validID) {
+
+        // Generate random key
+        for ( var i = 0; i < length; i++ ) {
+            connectionIDArray.push(
+                characters.charAt(Math.floor(Math.random() * characters.length))
+            );
+        }
+        connectionID = connectionIDArray.join('');
+
+        // Test against Business database
+        Business.findOne({connection_id: connectionID}, async (err, doc) => {
+            if (err) throw err;
+            else if (!doc) {
+                console.log('Unique connection id found');
+                validID = true;
+            } else {
+                'This ID is not unique'
+            }
+        });
+    }
+
+    return connectionID;
+}
+
 exports.generateStreamKey = generateStreamKey;
+exports.generateConnectionID = generateConnectionID;
