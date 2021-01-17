29b58e09a95b55ac56094a8f165340af58096f97
RobbyB97 <bergersr97@gmail.com>
2021-01-17 06:39:35 -0500

    update config for rtmp server

--- server/config/default.js
+++ server/config/default.js
@@ -8,28 +8,31 @@ const config = {
         allow_origin: '*',
     },
     rtmp: {
-        port: 1935,
-        chunk_size: 60000,
-        gop_cache: true,
-        ping: 60,
-        ping_timeout: 30,
+        rtmp: {
+            logType: 3,
+            port: 1935,
+            chunk_size: 60000,
+            gop_cache: true,
+            ping: 60,
+            ping_timeout: 30,
+            trans: {
+                ffmpeg: process.env.FFMPEG_PATH,
+                tasks: [
+                    {
+                        app: 'live',
+                        hls: true,
+                        hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
+                        dash: true,
+                        dashFlags: '[f=dash:window_size=3:extra_window_size=5]'
+                    }
+                ]
+            }
+        },
         http: {
             port: 8888,
             mediaroot: './server/media',
             allow_origin: '*'
         },
-        trans: {
-            ffmpeg: process.env.FFMPEG_PATH,
-            tasks: [
-                {
-                    app: 'live',
-                    hls: true,
-                    hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
-                    dash: true,
-                    dashFlags: '[f=dash:window_size=3:extra_window_size=5]'
-                }
-            ]
-        }
     },
     mongodb: {
         dbpath: './server/database/db',
