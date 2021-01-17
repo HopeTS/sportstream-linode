9374470c69956a7a81792b1273c4bb7b670098cf
RobbyB97 <bergersr97@gmail.com>
2021-01-17 06:43:17 -0500

    put transcoding config in right spot

--- server/config/default.js
+++ server/config/default.js
@@ -3,10 +3,6 @@
  */
 
 const config = {
-    http: {
-        media_root: './server/media',
-        allow_origin: '*',
-    },
     rtmp: {
         rtmp: {
             logType: 3,
@@ -14,25 +10,25 @@ const config = {
             chunk_size: 60000,
             gop_cache: true,
             ping: 60,
-            ping_timeout: 30,
-            trans: {
-                ffmpeg: process.env.FFMPEG_PATH,
-                tasks: [
-                    {
-                        app: 'live',
-                        hls: true,
-                        hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
-                        dash: true,
-                        dashFlags: '[f=dash:window_size=3:extra_window_size=5]'
-                    }
-                ]
-            }
+            ping_timeout: 30
         },
         http: {
-            port: 8888,
+            port: 8000,
             mediaroot: './server/media',
             allow_origin: '*'
         },
+        trans: {
+            ffmpeg: process.env.FFMPEG_PATH,
+            tasks: [
+                {
+                    app: 'live',
+                    hls: true,
+                    hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
+                    dash: true,
+                    dashFlags: '[f=dash:window_size=3:extra_window_size=5]'
+                }
+            ]
+        }
     },
     mongodb: {
         dbpath: './server/database/db',
