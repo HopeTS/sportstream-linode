4a236042a0efb6bed18d9fb850bf74bd77cd5a44
RobbyB97 <bergersr97@gmail.com>
2021-01-17 20:27:55 -0500

    fix async error

--- server/routers/streams.js
+++ server/routers/streams.js
@@ -50,7 +50,7 @@ router.get('/streams/user-to-business', ensureLoggedIn(), async (req, res) => {
                                 if (bus) {
                                     console.debug('Here is the connected business', bus);
                                     const hashedKeys = []
-                                    bus.stream_key.forEach(async (stream_key) => {
+                                    await bus.stream_key.forEach(async (stream_key) => {
                                         console.log('Here is the stream_key', stream_key)
                                         const hashedKey = await bcrypt.hash(stream_key, 10);
                                         console.log('New hashed key', hashedKey);
