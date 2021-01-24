b83f5a642e2fc0ace1cb77e9bb0f6524220c5af0
RobbyB97 <bergersr97@gmail.com>
2021-01-24 00:40:30 -0500

    CW-12 Create Stream schema

---
+++ server/database/schema/StreamSchema.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const { Business } = require('./Schema');
+
+
+/**
+ * Schema for stream objects
+ * 
+ * __field:__ Field name
+ * 
+ * __business:__ Associated Business (id)
+ * 
+ * __status:__ Upcoming stream, current stream or previous stream
+ */
+const StreamSchema = new Schema({
+    field: {
+        type: String,
+        required: true
+    },
+    business: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['upcoming', 'current', 'previous'],
+        required: true
+    }
+});
+
+/**
+ * Sets stream status to current
+ * 
+ * @param {*} cb callback function
+ */
+StreamSchema.methods.startStream = async function(cb) {
+    this.status = 'current';
+    return await this.save(cb);
+}
+
+/**
+ * Sets stream status to previous
+ * 
+ * @param {*} cb callback function
+ */
+StreamSchema.methods.endStream = async function(cb) {
+    this.status = 'previous';
+    return await this.save(cb);
+}
+
+module.exports = StreamSchema;
