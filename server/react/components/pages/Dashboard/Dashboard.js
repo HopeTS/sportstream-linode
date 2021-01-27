6554ab9a19a9eaa662444ce013f78b44f334bed2
RobbyB97 <bergersr97@gmail.com>
2021-01-27 06:46:41 -0500

    CW-34 set up dashboard components

---
+++ server/react/components/pages/Dashboard/Dashboard.js
@@ -0,0 +1,38 @@
+import React, {useEffect, useState} from 'react';
+import {connect} from 'react-redux';
+import {NavLink} from 'react-router-dom';
+
+import config from '../../../config/default';
+import {page_ID__Set} from '../../../redux/actions/page';
+import BusinessDashboard from './BusinessDashboard';
+import UserDashboard from './UserDashboard';
+
+export function Dashboard() {
+    return (
+        <div className="Dashboard">
+            {props.account.type}
+            {props.account.type === 'user' ?
+                <UserDashboard />
+                :
+                <BusinessDashboard />
+            }
+        </div>
+    );
+}
+
+
+/* Connect to store */
+const mapStateToProps = (state) => {
+    return {
+        isAuthenticated: state.auth.isAuthenticated,
+        account: state.auth.account
+    };
+};
+
+const mapDispatchToProps = (dispatch) => ({
+    page_ID__Set: (id) => {
+        dispatch(page_ID__Set(id));
+    }
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
---
+++ server/react/components/pages/Dashboard/UserDashboard.js
@@ -0,0 +1,29 @@
+import React, {useEffect, useState} from 'react';
+import {connect} from 'react-redux';
+import {NavLink} from 'react-router-dom';
+
+import config from '../../../config/default';
+
+export function UserDashboard() {
+    return (
+        <div className="UserDashboard">
+            {props.account.type}
+        </div>
+    );
+}
+
+
+/* Connect to store */
+const mapStateToProps = (state) => {
+    return {
+        account: state.auth.account
+    };
+};
+
+const mapDispatchToProps = (dispatch) => ({
+    page_ID__Set: (id) => {
+        dispatch(page_ID__Set(id));
+    }
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserDashboard);
