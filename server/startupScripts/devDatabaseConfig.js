acaf0792996fdd6f222044867336f6fb9e1594be
RobbyB97 <bergersr97@gmail.com>
2021-01-30 20:07:30 -0500

    trying to fix random import problem

--- server/startupScripts/devDatabaseConfig.js
+++ server/startupScripts/devDatabaseConfig.js
@@ -11,7 +11,7 @@ const Stream = require('../database/schema/Schema').Stream;
 /**
  * Configure database for development
  */
-export default async () => {
+export async function devDatabaseConfig() {
     // Test users
     const users = [{
         name: 'Test1',
@@ -204,3 +204,5 @@ export default async () => {
 
     return true;
 }
+
+module.exports = devDatabaseConfig;
