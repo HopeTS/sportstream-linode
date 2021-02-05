f63fdf1a6a2e7b3c1f4ec96f018577ca07637979
RobbyB97 <bergersr97@gmail.com>
2021-02-05 05:46:08 -0500

    Remove watch page

--- server/react/config/navigation.js
+++ server/react/config/navigation.js
@@ -1,38 +1,47 @@
 /**
- *    This array contains all the links displayed in the mobile and desktop
- *    navigation ui components.
+ * This array contains all the links displayed in the mobile and desktop
+ * navigation ui components
+ * 
+ *  Link attributes:
+ *  ----------------
+ *  href: string = the internal route or external link
+ * 
+ *  text: string = the label of the navigation component
+ * 
+ *  internal: bool = whether href is internal or external
+ * 
+ *  auth: object (todo) = {
+ * 
+ *      strict: bool = whether auth_status matters
+ * 
+ *      auth_status: bool = whether or not user is logged in
+ * 
+ *  }
+ *  sublinks: array = [
+ * 
+ *      sublink: object = {
+ * 
+ *          text: string = the label of the sublink
+ * 
+ *          href: string = the internal or external link
+ * 
+ *          internal: bool = whether href is internal or external
+ * 
+ *          auth: object (todo) = {
+ * 
+ *              strict: bool = whether auth_status matters
+ * 
+ *              auth_status: bool = whether or not user is logged in
+ * 
+ *          }
+ *      }
+ *  ]
  */
-
 export default [
-    /**
-     *  Link attributes:
-     *  ----------------
-     *  href: string = the internal route or external link
-     *  text: string = the label of the navigation component
-     *  internal: bool = whether href is internal or external
-     *  auth: object (todo) = {
-     *      strict: bool = whether auth_status matters
-     *      auth_status: bool = whether or not user is logged in
-     *  }
-     *  sublinks: array = [
-     *      sublink: object = {
-     *          text: string = the label of the sublink
-     *          href: string = the internal or external link
-     *          internal: bool = whether href is internal or external
-     *          auth: object (todo) = {
-     *              strict: bool = whether auth_status matters
-     *              auth_status: bool = whether or not user is logged in
-     *          }
-     *      }
-     *  ]
-     */
+    
     {
         href: "/",
         text: "Home",
-        internal: true
-    }, {
-        href: "/watch",
-        text: "Watch",
         internal: true
     }
 ]
