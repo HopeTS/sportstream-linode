f1c9ac806ab371271f377009d766fbb87e1d9883
RobbyB97 <bergersr97@gmail.com>
2021-02-08 05:35:41 -0500

    pipe endpoint

--- server/routers/stream.js
+++ server/routers/stream.js
@@ -47,22 +47,55 @@ router.get('/stream/hls/:key', ensureLoggedIn(), async (req, res) => {
     try {
         console.log('Stream key listed:', req.params.key);
 
+        const hlsUrl = `http://localhost:8000/live/${req.params.key}/index.m3u8`;
+        request(hlsUrl).pipe(res);
+
         // Set headers
-        res.header("Access-Control-Allow-Origin", nmsConfig.http.allow_origin);
+        /* res.header("Access-Control-Allow-Origin", nmsConfig.http.allow_origin);
         res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
         res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-        res.header("Access-Control-Allow-Credentials", true);
+        res.header("Access-Control-Allow-Credentials", true); */
 
-        // Get HLS stream
-        const hlsRequest = http.request({
+        /* const options = {
+            port: '8000',
+            hostname: 'localhost',
+            path: `/${req.paramas.key}/index.m3u8`,
+            headers: req.headers
+        };
 
-        });
+        const callback = http.request(options, pres => {
+            res.writeHead(pres.statusCode);
+
+            pres.on('data', (chunk) => {
+                res.write(chunk);
+            })
+
+            pres.on('close', () => {
+                res.end();
+            })
+
+            pres.on('end', () => {
+                res.end();
+            })
+        })
+        .on('error', (e) => {
+            console.error(e.message);
+            try {
+                // attempt to set error message and http status
+                ores.writeHead(500);
+                ores.write(e.message);
+            } catch (e) {
+                // ignore
+            }
+            res.end();
+        }) */
+
         /* return res.status(206).send(
             `http://localhost:8000/live/${req.params.key}/index.m3u8`
         ); */
-        return res.status(206).redirect(
+        /* return res.status(206).redirect(
             `http://localhost:8000/live/${req.params.key}/index.m3u8`
-        );
+        ); */
     }
 
     catch(e) {
