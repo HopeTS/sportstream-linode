e7ff7684e95b8a0555470bfc2003279908d9b9c0
RobbyB97 <bergersr97@gmail.com>
2021-02-08 23:00:55 -0500

    try deleting audio

--- server/react/components/VideoPlayer.js
+++ server/react/components/VideoPlayer.js
@@ -56,6 +56,7 @@ export function VideoPlayer(props) {
                 isLive={true}
                 handleError={(err) => handle_http_video_error(err)}
                 enableStashBuffer={false}
+                hasAudio={false}
             />
 
             {/* If nothing works */}
