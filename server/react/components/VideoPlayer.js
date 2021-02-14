af1e22829278cbc3d62a74e34c5392951f1d8a2b
RobbyB97 <bergersr97@gmail.com>
2021-02-14 18:25:13 -0500

    explicitly set type

--- server/react/components/VideoPlayer.js
+++ server/react/components/VideoPlayer.js
@@ -14,9 +14,6 @@ export function VideoPlayer(props) {
 
     useEffect(() => {
         console.log('Component mounted')
-        
-        const video = document.querySelector('video');
-        video.setAttribute('type', 'video/mp4')
     
         axios.get(`https://${window.location.hostname}:8443/live/${props.streamKey}.flv`)
         .then((res) => {
@@ -61,6 +58,7 @@ export function VideoPlayer(props) {
                 handleError={(err) => handle_http_video_error(err)}
                 hasAudio={false}
                 isMuted={true}
+                type="mp4"
             />
 
             {/* If nothing works */}
