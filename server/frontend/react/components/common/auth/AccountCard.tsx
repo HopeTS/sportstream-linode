fa778f2676863ae942cdcaf736cf1d747840da4c
RobbyB97 <bergersr97@gmail.com>
2021-03-06 14:40:33 -0500

    ja -> tsx

--- server/frontend/react/components/common/auth/AccountCard.tsx
+++ server/frontend/react/components/common/auth/AccountCard.tsx
@@ -1,34 +1,35 @@
+export {};
 import React from 'react';
 import {connect} from 'react-redux';
-import {NavLink} from 'react-router-dom';
 
 import {
-    navMenu_Mobile__Off, accountMenu__Toggle, accountMenu__Off, accountMenu__On
+    navMenu_Mobile__Off, accountMenu__Toggle
 } from '../../../redux/actions/ui';
 import AccountCardAuth from './AccountCardAuth';
 import AccountCardNoAuth from './AccountCardNoAuth';
 
 
-/**
- * Master AccountCard component. Contains the login/sign up buttons for 
- * unauthenticated users and account picture and menu for authenticated users.
- * 
- * @prop mobile_nav: Whether or not mobile nav menu is up
- */
+/** Root AccountCard component */
 export class AccountCard extends React.Component {
-    constructor(props) {
+
+    state: {
+        auth: boolean
+    };
+    props: any;
+
+
+    constructor(props: any) {
         super(props);
         this.state = {
-            auth: false,
-        }
+            auth: false
+        };
     }
 
-    /**
-     * Toggles the mobile nav redux state if user clicks a link in the
-     * AccountCard while the mobile  nav is up
-     */
-    mobile_nav = () => {
+
+    /** Toggles mobile nav redux state */
+    private mobile_nav(): void {
         if (this.props.mobile_nav) {
+
             // Disable scrolling
             const html = document.getElementsByTagName('HTML')[0];
             html.setAttribute('data-Mobile_Nav', 'false');
@@ -36,15 +37,10 @@ export class AccountCard extends React.Component {
             // Dispatch
             this.props.navMenu_Mobile__Off();
         }
+
         return;
     }
 
-    /**
-     * Toggles the account card menu display
-     */
-    accountMenu__Toggle = () => {
-        this.props.accountMenu__Toggle();
-    }
 
     render() {
         return (
@@ -52,7 +48,7 @@ export class AccountCard extends React.Component {
                 id="AccountCard" 
                 data-mobile_nav={this.props.mobile_nav}
                 data-active={this.props.accountMenu}
-                onClick={this.props.navMenu_Mobile__Off}
+                onClick={this.mobile_nav}
             >
                 {this.props.isAuthenticated ?
                     <AccountCardAuth />
@@ -62,11 +58,11 @@ export class AccountCard extends React.Component {
             </div>
         );
     };
-};
+}
 
 
-/* Connect to store */
-const mapStateToProps = (state) => {
+// Connect to store
+const mapStateToProps = (state: any): any => {
     return {
         mobile_nav: state.ui.navMenu_Mobile,
         accountMenu: state.ui.accountMenu,
