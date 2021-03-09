edccab7f9c8abcf95f5a79e0ec0b9f3d21541d5d
RobbyB97 <bergersr97@gmail.com>
2021-03-09 09:12:28 -0500

    js -> ts

---
+++ server/frontend/react/components/pages/Wildcard.tsx
@@ -0,0 +1,33 @@
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+
+import { page_ID__Set } from '../../redux/actions/page';
+
+
+// Store config
+const mapDispatchToProps = (dispatch) => ({
+    page_ID__Set: (id) => {
+        dispatch(page_ID__Set(id));
+    }
+});
+
+
+/**  Wildcard page */
+const Wildcard = connect(
+    undefined, mapDispatchToProps
+)(function(props: any) {
+
+    useEffect(() => {
+        props.page_ID__Set('Wildcard');
+    }, []);
+
+
+    return(
+        <div id="Wildcard">
+
+        </div>
+    );
+});
+
+
+export = Wildcard;
