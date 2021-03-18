d7bcd515426af76903213b27a221f5c9289fabee
RobbyB97 <bergersr97@gmail.com>
2021-03-18 08:29:32 -0400

    no default exports

--- server/frontend/react/components/common/auth/AccountCardNoAuth.tsx
+++ server/frontend/react/components/common/auth/AccountCardNoAuth.tsx
@@ -1,45 +1,33 @@
-export {};
 import React from 'react';
 import {NavLink} from 'react-router-dom';
 
 
 /** AccountCard content for unauthenticated users */
-export class AccountCardNoAuth extends React.Component {
-    
-    props: any;
+function AccountCardNoAuth(props: any) {
+    return (
+        <div className="AccountCardNoAuth">
+            <div className="AccountCardNoAuth__links">
+                <NavLink
+                    className="AccountCardNoAuth__login"
+                    to="/login"
+                >
+                    <div>
+                        Log in
+                    </div>
+                </NavLink>
 
-    
-    constructor(props) {
-        super(props);
-    }
-
-
-    render() {
-        return (
-            <div className="AccountCardNoAuth">
-                <div className="AccountCardNoAuth__links">
-                    <NavLink
-                        className="AccountCardNoAuth__login"
-                        to="/login"
-                    >
-                        <div>
-                            Log in
-                        </div>
-                    </NavLink>
-
-                    <NavLink
-                        className="AccountCardNoAuth__register"
-                        to="/register"
-                    >
-                        <div>
-                            Register
-                        </div>
-                    </NavLink>
-                </div>
+                <NavLink
+                    className="AccountCardNoAuth__register"
+                    to="/register"
+                >
+                    <div>
+                        Register
+                    </div>
+                </NavLink>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 
-export default AccountCardNoAuth;
+export = AccountCardNoAuth;
